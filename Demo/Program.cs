aae11898828533e7094dedf8d55d01d0193876a5
moomen mohammmed <monm73530@gmail.com>
2024-12-20 18:52:28 +0200

    for-foreach

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -5,11 +5,36 @@
         static void Main(string[] args)
         {
             #region loop statment
-            for (int i = 1; i <= 10; i++)
-            {
-                Console.WriteLine($"{i} T");
-            }
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine($"{i} T");
+            //}
             #endregion
-        }
+
+            #region for-foreach
+            int[] Numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            //for (int i = 0; i < Numbers.Length; i++)
+            //{
+            //    Console.WriteLine(Numbers[i]);
+            //}
+
+
+            //foreach (int Number in Numbers)
+            //{
+            //    Console.WriteLine(Number);
+            //}
+
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                //Numbers[i] += 10;
+                if (Numbers[i] == 5)
+                {
+                    break;
+                }
+                Console.WriteLine(Numbers[i]);
+            }
+        #endregion
+    }
     }
 }
