2b561794bf98789b790fa6e8009d59fac2a2b443
moomen mohammmed <monm73530@gmail.com>
2024-12-20 14:07:55 +0200

    2-Write a program that allows the user to insert an integer then print a multiplication table up to 12

--- Assignment 4/Program.cs
+++ Assignment 4/Program.cs
@@ -6,26 +6,46 @@
         {
 
             #region  1- Write a program that allows the user to insert an integer then print all numbers between 1 to that number
-            Console.Write("Enter an Number: ");
-             string NumberInput = Console.ReadLine();
+            /* Console.Write("Enter an Number: ");
+              string NumberInput = Console.ReadLine();
 
-             if (int.TryParse(NumberInput, out int Num) && Num > 0)
-             {
-                 Console.WriteLine("Output:");
-                 for (int i = 1; i <= Num; i++)
-                 {
-                     Console.Write(i);
-                     if (i < Num)
-                     {
-                         Console.Write(",");
-                     }
-                 }
-             }
-         
+              if (int.TryParse(NumberInput, out int Num) && Num > 0)
+              {
+                  Console.WriteLine("Output:");
+                  for (int i = 1; i <= Num; i++)
+                  {
+                      Console.Write(i);
+                      if (i < Num)
+                      {
+                          Console.Write(",");
+                      }
+                  }
+              }
+            */
             #endregion
 
             #region 2-Write a program that allows the user to insert an integer then print a multiplication table up to 12
 
+            Console.Write("Enter a Number: ");
+            string NumberInput = Console.ReadLine();
+
+            // Parse the input into an integer
+            if (int.TryParse(NumberInput, out int number))
+            {
+                Console.WriteLine($"Number is {number}:");
+                for (int i = 1; i <= 12; i++)
+                {
+                    Console.Write(number * i);
+                    if (i < 12)
+                    {
+                        Console.Write(" , ");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please Enter a Number");
+            }
             #endregion
 
         }
