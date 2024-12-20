1ea4e10785886ff2b465ea485fb62874bf74f1f8
moomen mohammmed <monm73530@gmail.com>
2024-12-20 14:00:16 +0200

    1-Write a program that allows the user to insert an integer then print all numbers between 1 to that number

--- Assignment 4/Program.cs
+++ Assignment 4/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region  1- Write a program that allows the user to insert an integer then print all numbers between 1 to that number
+            Console.Write("Enter an Number: ");
+             string NumberInput = Console.ReadLine();
+
+             if (int.TryParse(NumberInput, out int Num) && Num > 0)
+             {
+                 Console.WriteLine("Output:");
+                 for (int i = 1; i <= Num; i++)
+                 {
+                     Console.Write(i);
+                     if (i < Num)
+                     {
+                         Console.Write(",");
+                     }
+                 }
+             }
+         
+            #endregion
+
+            #region 2-Write a program that allows the user to insert an integer then print a multiplication table up to 12
+
+            #endregion
+
         }
     }
 }
